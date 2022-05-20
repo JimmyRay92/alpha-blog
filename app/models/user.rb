f55ecb1fb7ec415db29f2cc26f22b3d615ee9d4f
jimmyray92 <jameswang675203@gmail.com>
2022-05-20 17:41:15 +1000

    changed devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
-class User < ApplicationRecord 
+class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    before_save { self.email = email.downcase }
-    has_secure_password
-    has_many :articles
-    validates :username, presence: true, 
-                        uniqueness: { case_senitive: false},
-                        length: { minimum: 3, maximum: 25}
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, 
-                        uniqueness: { case_senitive: false},
-                        length: { maximum: 105},
-                        format: { with: VALID_EMAIL_REGEX}
+
+  has_one :teacher, dependent: :destroy
+  has_many :bookings
+
+  # Validates Full Name for user sign-up
+  validates :fullname, presence: true, length: { in: 1...150 }
+
+  # Validates email for User sign-up
+  validates :email, presence: true, uniqueness: { case_sensitive: true }, length: { in: 1...75 }
+
+  # Validates password for user sign-up
+  validates :password, confirmation: true, presence: true, length: { in: 1...50 }
+
 end
