9c9359cb1eeb900bb084345bd755676b975dccff
jimmyray92 <jameswang675203@gmail.com>
2022-05-20 16:54:45 +1000

    styles model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_20_065035) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_20_065416) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -26,6 +26,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_20_065035) do
     t.datetime "updated_at", null: false
     t.index ["teacher_id"], name: "index_bookings_on_teacher_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+    t.boolean "speciality"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teachers", force: :cascade do |t|
